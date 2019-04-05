3700c1704f31a1afe44e64a149b7fff7bc781e6d
Matthew Bartl <mattbartl@gmail.com>
2019-04-05 20:59:58 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -5,9 +5,7 @@ class Shoe
 
   def initialize(brand)
     @brand = brand
-    if !BRANDS.include?(brand)
-      BRANDS << brand
-    end
+    if !BRANDS.include?(brand) {BRANDS << brand}
   end
 
   def cobble
